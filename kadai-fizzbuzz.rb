5f6bb53630bd6b524d6d9c5e6079c59d917b3268
ryo.suzuki <ottopiza@gmail.com>
2021-06-10 08:27:15 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,20 @@
 num_max = 100
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz "
+     "Fizz "
   elsif num % 5 == 0
-    puts "Buzz "
+     "Buzz "
   elsif (num % 3 == 0) && (num % 5 == 0)
-    puts "FizzBuzz "  
+     "FizzBuzz "  
   else
-    puts num.to_s
+     num.to_s
   end
 end
 
   (1..100).each do |n|
     puts fizzbuzz(n)
 end
+
 
 # 以下は動作確認用の記述です
 puts fizzbuzz(1)
